203476f8842dd4bcecca6161c5b8d4dec5fee4b8
Alex Hughes <hughes.13.alex@gmail.com>
2019-02-04 18:54:08 -0800

    added while loops

--- a2_1.cpp
+++ a2_1.cpp
@@ -1,6 +1,6 @@
 /*
 Alex Hughes
-1/30/2019
+2/3/2019
 
 Assignment 2.1
 A command line game that plays a simple version of blackjack.
@@ -9,6 +9,9 @@ A command line game that plays a simple version of blackjack.
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
 
 using namespace std;
 
@@ -20,10 +23,11 @@ int getRandom() {
 
 
 int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
 
-    int count, firstCards, currentCards, cardTotal;
-    int firstRandom = getRandom(), secondRandom = getRandom(), thirdRandom = getRandom();
-    string playAgain, answer;
+    int currentCards, cardTotal;
+    int firstRandom = getRandom(), secondRandom = getRandom(), loopRandom = getRandom();
+    string playAgain, newCard, newGame;
 
     // Player score
     cout << "First Cards: " << firstRandom << ", " << secondRandom << endl;
@@ -32,15 +36,42 @@ int main() {
 
     // Ask user for input
     cout << "Do you want another card? (y/n): " << endl;
-    cin >> answer;
+    cin >> newCard;
 
     // loop for getting another card
-    if (answer == "y") {
-        cout << "Card: " << thirdRandom << endl;
-        cout << "Total: " << currentCards + thirdRandom << endl;
+    while (newCard == "y" && currentCards < 21) {
+        int newNum = getRandom();
+        
+        cout << "Card: " << newNum << endl;
+        cout << "Total: " << currentCards + newNum << endl;
+        cout << "Do you want another card? (y/n): " << endl;
+        cin >> newCard;
     }
 
+    cardTotal = currentCards + loopRandom;
 
+    // loop for ending game
+    while (newCard == "n") {
+        if (cardTotal > 21) {
+            cout << "Bust!" << endl;
+        }
+        else if (cardTotal == 21) {
+            cout << "Win!" << endl;
+        }
+        else if (cardTotal < 21) {
+            cout << "Thanks for playing" << endl;
+        }
+        break;
+    }
 
-    return 0;
+    // new game
+    cout << "Would you like to play again? (y/n): " << endl;
+    cin >> playAgain;
+    if (playAgain == "y") {
+         cout << "some text:" << endl;
+
+    }
+
+    return 0; 
 }
+
